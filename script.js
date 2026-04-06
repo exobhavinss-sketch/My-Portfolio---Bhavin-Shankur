016f55fa20df36324690fd79fe8640590a1b61a4
exobhavinss-sketch <exobhavinss@gmail.com>
2026-04-06 14:36:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 // smooth scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-anchor.addEventListener("click", function(e) {
+document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+anchor.addEventListener("click",function(e){
 e.preventDefault();
 document.querySelector(this.getAttribute("href"))
 .scrollIntoView({
-behavior: "smooth"
+behavior:"smooth"
 });
 });
 });
 
-// open certificate
-function openImage(src){
-window.open(src, "_blank");
+
+// dark mode
+const toggle=document.getElementById("darkModeToggle");
+
+toggle.onclick=()=>{
+document.body.classList.toggle("dark");
+}
+
+
+// lightbox gallery
+const images=document.querySelectorAll(".gallery img");
+const lightbox=document.getElementById("lightbox");
+const lightImg=document.getElementById("lightbox-img");
+const close=document.getElementById("close");
+
+images.forEach(img=>{
+img.addEventListener("click",()=>{
+lightbox.style.display="flex";
+lightImg.src=img.src;
+});
+});
+
+close.onclick=()=>{
+lightbox.style.display="none";
 }
